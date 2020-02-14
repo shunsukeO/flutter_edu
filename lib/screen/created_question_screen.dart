843f99f5d523a8f610ba7bc854d29dec71635a4c
shunsuke <shunsuke@shunsukenoMacBook-Pro.local>
2020-02-14 13:50:01 +0900

    作成問題一覧画面修正、コメント追記

--- lib/screen/created_question_screen.dart
+++ lib/screen/created_question_screen.dart
@@ -5,31 +5,33 @@ import 'package:my_education/blocs/question_bloc.dart';
 class CreatedQuestionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Provider = blocのオブジェクトを受け取る
     return Provider<QuestionBloc> (
+      // initStateのようなもの
       create: (_) => QuestionBloc(),
       child: _QuestionScreenTemplate(),
       dispose: (_, value) => value.dispose(),
     );
-    // return Scaffold(
-    //   backgroundColor: Colors.green[100],
-    //   // Appbar
-    //   appBar: AppBar(
-    //     backgroundColor: Colors.green[100],
-    //     title: Text(
-    //       '作成問題一覧',
-    //       style: TextStyle(fontSize: 16),
-    //     ),
-    //   ),
-    //   body: Center(
-    //     child: Icon(Icons.mode_edit),
-    //   )
-    // );
   }
 }
 
 class _QuestionScreenTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+    final QuestionBloc _bloc = Provider.of<QuestionBloc>(context);
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      // Appbar
+      appBar: AppBar(
+        backgroundColor: Colors.green[100],
+        title: Text(
+          '作成問題一覧',
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+      body: Center(
+        child: Icon(Icons.mode_edit),
+      )
+    );
   }
 }
