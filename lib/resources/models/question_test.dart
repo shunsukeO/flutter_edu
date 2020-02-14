e8261f23ef4e64f56ff67a4af2a895daf250f620
shunsuke <shunsuke@shunsukenoMacBook-Pro.local>
2020-02-14 16:16:55 +0900

    作成問題テンプレのリスト表示

--- lib/resources/models/question_test.dart
+++ lib/resources/models/question_test.dart
@@ -2,4 +2,11 @@ class Question {
   String questionId;
   String userId;
   String title;
+
+  Question({ this.questionId, this.userId, this.title });
+  Question.init({ this.questionId, this.userId }) {
+    questionId = '';
+    userId = '';
+    title = '';
+  }
 }
--- lib/screen/created_question_screen.dart
+++ lib/screen/created_question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:my_education/blocs/question_bloc.dart';
+import 'package:my_education/resources/models/question_test.dart';
 
 class CreatedQuestionScreen extends StatelessWidget {
   @override
@@ -18,7 +19,6 @@ class CreatedQuestionScreen extends StatelessWidget {
 class _QuestionScreenTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final QuestionBloc _bloc = Provider.of<QuestionBloc>(context);
     return Scaffold(
       backgroundColor: Colors.green[100],
       // Appbar
@@ -29,8 +29,32 @@ class _QuestionScreenTemplate extends StatelessWidget {
           style: TextStyle(fontSize: 16),
         ),
       ),
-      body: Center(
-        child: Icon(Icons.mode_edit),
+      body: _CreatedQuestionListComponent(),
+    );
+  }
+}
+
+// 作成問題一覧リスト
+class _CreatedQuestionListComponent extends StatelessWidget {
+  Question _question;
+  var listItem = ["TOEIC850点問題集", "ドイツ語検定3級問題", "フランス語検定"];
+
+  @override
+  Widget build(BuildContext context) {
+    final QuestionBloc _bloc = Provider.of<QuestionBloc>(context);
+
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              child: Text(listItem[index], style: TextStyle(fontSize: 20),),
+              padding: EdgeInsets.all(40.0),
+            )
+          );
+        },
+        itemCount: listItem.length,
       )
     );
   }
